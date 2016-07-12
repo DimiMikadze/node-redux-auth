673d87a2397eef62bea528665cf47e9e87c5eb37
Dimitri Mikadze <dimitrimikadze@gmail.com>
2016-07-12 23:38:55 +0400

    update api url

--- client/src/app/config.js
+++ client/src/app/config.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? 'http://dimitrimikadze.com' : 'http://localhost:3333';
+export const API_URL = process.env.NODE_ENV === 'production' ? 'http://dimitrimikadze.com:3333' : 'http://localhost:3333';
