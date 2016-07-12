3aa1d2f4681488e3a04737ba54823650129d5d22
Dimitri Mikadze <dimitrimikadze@gmail.com>
2016-07-12 23:26:12 +0400

    add example config

---
+++ server/src/config/example.index.js
@@ -0,0 +1,12 @@
+export const dbConfig = {
+  secret: 'SomeRandomSecretString',
+  db: 'mongodb://localhost:auth/auth',
+};
+
+export const emailConfig = {
+  service: 'Gmail',
+  auth: {
+    user: 'email@gmail.com',
+    pass: 'Password',
+  },
+};
